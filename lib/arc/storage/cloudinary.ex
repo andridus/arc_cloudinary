e8b45adb76df2f75584309e1b6391dc6ef9b7eca
Helder de Sousa <helderhenri@gmail.com>
2021-06-06 14:14:39 -0300

    feat: add delete function

--- lib/arc/storage/cloudinary.ex
+++ lib/arc/storage/cloudinary.ex
@@ -6,8 +6,11 @@ defmodule Arc.Storage.Cloudinary do
     full_path = Path.join(destination_dir, file.file_name)
 
     Cloudex.upload(file.path, %{ public_id: file.file_name })
-    |> IO.inspect
 
+  end
+
+  def delete(definition, version, {file, scope}) do
+    Cloudex.delete(file)
   end
 
   def url(definition, version, {file, scope}, options \\ []) do
